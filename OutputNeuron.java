116a4e793b69a2db9be61085fbe65f39247764c0
RebelKeithy <RebelKeithy@gmail.com>
2012-12-01 11:52:36 -0600

    Added the other types of neuton classes
    It HiddenHeuron should probably become just Neuron, and OutputNeuron and
    InputNeuron extend Neuron

---
+++ OutputNeuron.java
@@ -0,0 +1,35 @@
+
+public class OutputNeuron implements Neuron {
+
+	@Override
+	public Axon connect(Neuron other) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void disconnect(Neuron other) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean sendActionPotentialTo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean receiveActionPotentialFrom(Neuron preSynaptic,
+			double actionPotential) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double evaluate(double oldActionPotential) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
